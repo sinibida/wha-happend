4bf67aad68a3c0d9f579c0560bc60d9d825d1ffa
SPAUPA <40449398+sinibida@users.noreply.github.com>
2024-12-20 23:02:00 +0900

    *: nextConfig

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
+import createMDX from "@next/mdx";
 
 const withNextIntl = createNextIntlPlugin();
 
-const nextConfig: NextConfig = withNextIntl({
-  /* config options here */
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
 });
 
-export default nextConfig;
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+export default withNextIntl(withMDX(nextConfig));
